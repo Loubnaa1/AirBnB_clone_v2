88dd53cb6e1ef3ef2c9db0eec53c29b05d6cfc58
Loubnaa1 <loubnamerzak4@gmail.com>
2024-01-04 18:51:58 -0800

    my work

--- web_flask/10-hbnb_filters.py
+++ web_flask/10-hbnb_filters.py
@@ -1,6 +1,6 @@
 #!/usr/bin/python3
-"""script that starts a Flask web application:
-listening on 0.0.0.0, port 5000"""
+"""starts a Flask web application:
+listens on 0.0.0.0, port 5000"""
 from flask import Flask
 from flask import render_template
 from models import storage
@@ -12,16 +12,14 @@ app = Flask(__name__)
 
 @app.teardown_appcontext
 def teardown_app(exc):
-    """After each request you must remove the current SQLAlchemy Session"""
     storage.close()
 
 
 @app.route("/hbnb_filters", strict_slashes=False)
 def hbnb_filters():
-    """display a HTML page: (inside the tag BODY)"""
-    states_list = storage.all(State)
-    amenities_list = storage.all(Amenity)
-    return render_template("10-hbnb_filters.html", states_list=states_list, amenities_list=amenities_list)
+    states = storage.all(State)
+    amenities = storage.all(Amenity)
+    return render_template("10-hbnb_filters.html", states=states, amenities=amenities)
 
 
 if __name__ == "__main__":
--- web_flask/100-hbnb.py
+++ web_flask/100-hbnb.py
@@ -1,6 +1,6 @@
 #!/usr/bin/python3
-"""script that starts a Flask web application:
-listening on 0.0.0.0, port 5000"""
+"""starts a Flask web application:
+listens on 0.0.0.0, port 5000"""
 from flask import Flask
 from flask import render_template
 from models import storage
@@ -13,17 +13,15 @@ app = Flask(__name__)
 
 @app.teardown_appcontext
 def teardown_app(exc):
-    """After each request you must remove the current SQLAlchemy Session"""
     storage.close()
 
 
 @app.route("/hbnb", strict_slashes=False)
 def hbnb_filters():
-    """display a HTML page: (inside the tag BODY)"""
-    states_list = storage.all(State)
-    amenities_list = storage.all(Amenity)
-    places_list = storage.all(Place)
-    return render_template("100-hbnb.html", states_list=states_list, amenities_list=amenities_list, places_list=places_list)
+    states = storage.all(State)
+    amenities = storage.all(Amenity)
+    places = storage.all(Place)
+    return render_template("100-hbnb.html", states=states, amenities=amenities, places=places)
 
 
 if __name__ == "__main__":
